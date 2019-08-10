d69cc0a774acc67016688f18fce8a6abb07e471b
Lukas Lapinskas <51517974+LapinskasL@users.noreply.github.com>
2019-08-09 20:38:22 -0500

    changes in description...
    user can now create up to 10 tabs
    prepared code for implementing deletion feature

--- options/options.js
+++ options/options.js
@@ -1,10 +1,49 @@
 console.log("options.js running");
 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    switch(request.msg) {
+        case "newTab":
+            undoJavaScriptHTML();
+            initialLoad();
+            break;
+        case "removeTabSettings":
+            chrome.storage.sync.get({
+                tabIndex: -1,
+            }, function(items) {
+                removeTabSettings(items.tabIndex);
+            }); 
+            break;
+    }
+});
+
+// function scrollDown() {
+//     var testLists = document.getElementsByClassName('testList');
+//     var testListLast = testLists.length - 1
+//     testListLast[testListLast].scrollTop = testListLast[testListLast].scrollHeight;
+// }
+
+function removeTabSettings(index) {
+    console.log("removeTabSettings aunched with index: " + index);
+}
+
+function undoJavaScriptHTML() {
+    document.body.innerHTML = "";
+
+    var h3 = document.createElement("h3");
+    h3.innerHTML = "*Changes are saved automatically";
+
+    var br = document.createElement("br");
+    
+    document.body.appendChild(h3);
+    document.body.appendChild(br);
+}
+
 window.onload = function() {
-    //@@@TEMPORARY GLOBAL?????
-    textBoxesNum = document.getElementsByClassName('textbox').length;
+    initialLoad();
+}
 
 
+function setSettings() {
     restoreData();
 
     let testButtons = document.getElementsByClassName('testList');
@@ -19,21 +58,33 @@ window.onload = function() {
 }
 
 
+function initialLoad() {
+    chrome.storage.sync.get({
+        tabBlocks: 1,
+    }, function(items) {
+        for (let i = 0; i < items.tabBlocks; i++) {
+            createNewTabSettingsBlock(i + 1);
+        }
+        setSettings();
+    }); 
+}
+
 function saveData() {
     // by passing an object you can define default values e.g.: []
     chrome.storage.sync.get({
         listNames: [],
         websiteList: [],
+        tabBlocks: 1,
     }, function (items) {
-        var listNames = resizeArray(items.listNames, 3);
-        var websiteList = resizeArray(items.websiteList, 3)
+        var listNames = resizeArray(items.listNames, items.tabBlocks);
+        var websiteList = resizeArray(items.websiteList, items.tabBlocks);
         var listArr = document.getElementsByClassName('listName');
         var websiteListArr = document.getElementsByClassName('textbox');
 
         listNames = [];
         websiteList = [];
 
-        for (let i = 0; i < listArr.length; i++) {
+        for (let i = 0; i < items.tabBlocks; i++) {
             listNames.push(listArr[i].value);
             websiteList.push(websiteListArr[i].value);
         }
@@ -61,12 +112,13 @@ function restoreData() {
     chrome.storage.sync.get({
         websiteList: [],       
         listNames: [],
+        tabBlocks: 1,
     }, function(items) {
         var listArr = document.getElementsByClassName('listName');
         var textBoxes = document.getElementsByClassName('textbox');
 
-        var listNames = resizeArray(items.listNames, 3);
-        var websiteList = resizeArray(items.websiteList, 3)
+        var listNames = resizeArray(items.listNames, items.tabBlocks);
+        var websiteList = resizeArray(items.websiteList, items.tabBlocks);
         for (let i = 0; i < listArr.length; i++) {
             listArr[i].value = listNames[i];
             textBoxes[i].value = websiteList[i];
@@ -75,6 +127,45 @@ function restoreData() {
   }
 
 
+
+  function createNewTabSettingsBlock(listNum) {
+    var div = document.createElement("div");
+    div.className = "tabSettingsBlock";
+
+    var span = document.createElement("span");
+    span.className = "listNameSpan";
+    span.innerHTML = "List " + listNum + " name: ";
+
+    var input = document.createElement("input");
+    input.type = "text";
+    input.maxLength = "10";
+    input.className = "listName";
+
+    var textarea = document.createElement("textarea");
+    textarea.className = "textbox";
+    textarea.wrap = "off";
+    textarea.placeholder = "Paste each link on a new line";
+
+    var button = document.createElement("button");
+    button.className = "testList";
+    button.type = "button";
+    button.innerHTML = "Test List #" + listNum;
+
+    div.appendChild(span);
+    div.appendChild(input);
+    div.appendChild(document.createElement("br"));
+    div.appendChild(textarea);
+    div.appendChild(document.createElement("br"));
+    div.appendChild(button);
+
+    document.body.appendChild(div);
+    document.body.appendChild(document.createElement("br"));
+    document.body.appendChild(document.createElement("br"));
+    document.body.appendChild(document.createElement("br"));
+}
+
+
+//Duplicate function (same in popup.js). Fix later
 function resizeArray(arr, size) {
     arr.length = size;
     for (let i = 0; i < size; i++) {
--- popup/popup.js
+++ popup/popup.js
@@ -1,26 +1,86 @@
+//todo: implement deletion function
+
 console.log("popup.js running");
 
 window.onload = function() {
-    //@@@TEMPORARY GLOBAL?????
-    tabBlockNum = document.getElementsByClassName('tabBlock').length;
+    loadTabBlocks();
+    document.getElementById("plusIcon").addEventListener("click", addTab);
+    console.log("popup window.onload ran");
+};
 
+function loadTabBlocks() {
+    chrome.storage.sync.get({
+        tabBlocks: 1,
+    }, function(items) {
+        initialLoad(items.tabBlocks);
+    });  
+}
 
-    positionBlocks();
-
-    let tabIcons = document.getElementsByClassName('tabIcon');
-    for (let i = 0; i < tabIcons.length; i++) {
-        tabIcons[i].addEventListener("click", ()=> restoreAndLaunch(i));
+function initialLoad(numOfBlocks) {
+    for (let i = 0; i < numOfBlocks; i++) {
+        createNewTabBlock();
     }
 
-    restoreListNames();
+    retrieveTabInfo();
 
     let gearOptions = document.getElementById('gearIcon');
     gearOptions.addEventListener("click", function () {
         chrome.runtime.openOptionsPage();
     });
 
-    console.log("popup window.onload ran");
-};
+    let trashClosedIcon = document.getElementById('trashClosedIcon');
+    trashClosedIcon.addEventListener("click", function() {
+        trashClosedIcon.style.display = "none";
+        let trashOpenedIcon = document.getElementById('trashOpenedIcon');
+        trashOpenedIcon.style.display = "inline";
+        let deleteIcons = document.getElementsByClassName('deleteIcon');
+        for (let i = 0; i < deleteIcons.length; i++) {
+            deleteIcons[i].style.display = "inline";
+        }
+
+        trashOpenedIcon.addEventListener("click", function() {
+            trashOpenedIcon.style.display = "none";
+            trashClosedIcon.style.display = "inline";
+            for (let i = 0; i < deleteIcons.length; i++) {
+                deleteIcons[i].style.display = "none";
+            }
+        });
+    });
+}
+
+function retrieveTabInfo() {
+    var tabBlockNum = document.getElementsByClassName('tabBlock').length;
+    setNumOfTabBlocks(tabBlockNum);
+
+    let tabIcons = document.getElementsByClassName('tabIcon');
+    for (let i = 0; i < tabIcons.length; i++) {
+        tabIcons[i].addEventListener("click", ()=> restoreAndLaunch(i));
+    }
+    restoreListNames();
+}
+
+
+function setNumOfTabBlocks(num) {
+    chrome.storage.sync.set({
+        tabBlocks: num,
+    });
+}
+
+function addTab() {
+    chrome.storage.sync.get({
+        tabBlocks: 1,
+    }, function(items) {
+        if (items.tabBlocks <= 9) {
+            createNewTabBlock();
+            retrieveTabInfo();
+            chrome.runtime.sendMessage({
+                msg: "newTab",
+            });
+        }
+    }); 
+    
+}
+
 
 function nameIfEmpty(string, num) {
     if (string == "") {
@@ -33,9 +93,10 @@ function nameIfEmpty(string, num) {
 function restoreListNames() {
     chrome.storage.sync.get({
         listNames: [],
+        tabBlocks: 1,
     }, function(items) {
         let tabNames = document.getElementsByClassName('tabName');
-        let listNames = resizeArray(items.listNames, tabBlockNum);
+        let listNames = resizeArray(items.listNames, items.tabBlocks);
         console.log(listNames[0]);
         for (let i = 0; i < tabNames.length; i++) {
             tabNames[i].innerHTML = listNames[i];
@@ -47,9 +108,10 @@ function restoreAndLaunch(index) {
     console.log("restoring is happening"); 
     chrome.storage.sync.get({
         websiteList: [""],
+        tabBlocks: 1,
     }, function(items) {
-        let websiteList = resizeArray(items.websiteList, tabBlockNum);
-        websiteList.length = 3;
+        resizeArray(items.websiteList, items.tabBlocks);
+        //websiteList.length = 3;
         sendMessageToOptions(items.websiteList[index]);
     });
   }
@@ -65,19 +127,32 @@ function sendMessageToOptions(theList) {
     }
 }
 
-function positionBlocks() {
-    let tabBlocks = document.getElementsByClassName('tabBlock');
 
-    let left = 1;
-    for (let i = 0; i < tabBlocks.length; i++) {
-        tabBlocks[i].style.position = "absolute";
-        tabBlocks[i].style.top = "0.8em";
-        tabBlocks[i].style.left = left + "em";
-        left += 6;
-        console.log(left);
-    }
+function createNewTabBlock() {
+    var tabBlockDiv = document.createElement("div");
+    tabBlockDiv.className = "tabBlock";
+
+    var label = document.createElement("label");
+    label.className = "tabName";
+
+    var tabIconImg = document.createElement("img");
+    tabIconImg.className = "tabIcon";
+    tabIconImg.src = "../images/tab1.png";
+
+    var deleteIconImg = document.createElement("img");
+    deleteIconImg.className = "deleteIcon";
+    deleteIconImg.src = "../images/delete.png";
+
+    tabBlockDiv.appendChild(label);
+    tabBlockDiv.appendChild(document.createElement("br"));
+    tabBlockDiv.appendChild(tabIconImg);
+    tabBlockDiv.appendChild(deleteIconImg);
+
+    document.getElementById("tabBlockDivWrapper").appendChild(tabBlockDiv);
 }
 
+
+//Duplicate function (same in options.js). Fix later
 function resizeArray(arr, size) {
     if (arr.length !== size) {
         arr.length = size;
